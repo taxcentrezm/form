8875574b167462c3925399241fba84e89cd380fe
taxcentrezm <taxcentrezm@gmail.com>
2026-01-06 14:29:47 +0200

    dfg

--- api/tts.js
+++ api/tts.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 
     try {
         const response = await fetch(
-            "https://api-inference.huggingface.co/models/facebook/mms-tts-bem",
+            "https://router.huggingface.co/models/facebook/mms-tts-bem",
             {
                 headers: {
                     Authorization: `Bearer ${HF_TOKEN}`,
